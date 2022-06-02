4bedae0b0772ac860a3fbdacdfb2a78a6a0a2061
Maxime Cottin <maximecottin@Maximes-MacBook-Air.local>
2022-06-02 02:14:21 +0200

    [WIP] Improve Header + title next sections

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import Menu from "./components/Menu";
 import "./styles/App.css";
 
 function App() {
-	return (
-		<>
-			<Menu />
-			<Header />
-		</>
-	);
+  return (
+    <>
+      <Menu />
+      <Header />
+      <section className="w-screen px-6 overflow-x-hidden">
+        <h2 className="text-white font-bold text-2xl inline-block relative">
+          New & Trending
+        </h2>
+      </section>
+    </>
+  );
 }
 
 export default App;
